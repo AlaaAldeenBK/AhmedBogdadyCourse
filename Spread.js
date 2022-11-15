96b481ed8e4836d87977b10bb3ad34f0dc8e140a
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-11-15 17:52:36 +0300

    new file

--- Spread.js
+++ Spread.js
@@ -1,2 +1,16 @@
 let oldArray = [1,2,3,4,5];
 console.log(oldArray);
+let newArray = [10,11,12, ...oldArray];
+console.log(newArray);
+let oldObject ={
+    id:'1',
+    name:'alaa'
+}
+console.log(oldObject);
+let newObject = {...oldObject,name:'ahmed'};
+console.log(newObject);
+let sum = (...args)=>{
+    return args.filter(item => item ===1);
+
+}
+console.log(sum(2,3,5));
