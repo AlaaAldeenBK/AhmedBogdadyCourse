52a839d98a47cf56f814532472dcd1c95916b53e
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-11-16 17:17:42 +0300

    Object Destruction

--- destruction.js
+++ destruction.js
@@ -3,5 +3,9 @@
 const arr =[1,2,5,7];
 // const [num1, num2] = arr;
 const [, ,num3] = arr;
-console.log(num3);
+// console.log(num3);
 // console.log(num1, num2);
+
+const ob = {id : '1', name : "Alaa"}
+const {name, id} = ob;
+console.log(name, id);
