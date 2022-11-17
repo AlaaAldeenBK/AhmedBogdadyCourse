b74f04b45f42dee0d889f66f54431bb41a59aa68
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-11-18 00:48:11 +0300

    Array Method 2

---
+++ arrayMethod2.js
@@ -0,0 +1,9 @@
+// const arr1 = [1,2,3,4,5,6,7,8,9]
+// const arr2 = arr1.filter(item => item === 12);
+// console.log(arr1);
+// console.log(arr2);
+
+const arr1 = [1,2,3,4,5,6,7,8,9]
+const arr2 = arr1.filter(item => item === 12);
+console.log(arr1);
+console.log(arr2);
